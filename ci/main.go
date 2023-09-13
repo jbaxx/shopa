2dc2846ed75130dbe4a53c4b3524e046d06a3e03
jbaxx <jbaxx@users.noreply.github.com>
2023-09-12 22:22:53 -0500

    It works and exports to local

--- ci/main.go
+++ ci/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 
@@ -10,49 +9,72 @@ import (
 )
 
 func main() {
+
 	ctx := context.Background()
-	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Close()
 
-	source := client.Container()
-	source = source.From("golang:1.19")
-	source = source.WithDirectory(
-		"/app",
-		client.Host().Directory("."),
-		dagger.ContainerWithDirectoryOpts{
-			Exclude: []string{"ci/"},
-		},
-	)
+	// get reference to the local project
+	src := client.Host().Directory(".")
 
-	runner := source.WithWorkdir("/app")
-	out, err := runner.WithExec([]string{"go", "mod", "download"}).Stderr(ctx)
+	golang := client.Container().From("golang:latest")
+
+	// mount cloned repository into `golang` image
+	golang = golang.WithDirectory("/app", src).WithWorkdir("/app")
+
+	// define the application build command
+	path := "build/"
+	golang = golang.WithExec([]string{"go", "build", "-v", "-o", path})
+
+	// get a reference to build output directory in container
+	output := golang.Directory(path)
+
+	// write contents of container build/ directory to the host
+	_, err = output.Export(ctx, path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(out)
 
-	test := runner.WithWorkdir("/app")
-	out, err = test.WithExec([]string{"go", "test", "./..."}).Stderr(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(out)
+	// source := client.Container()
+	// source = source.From("golang:1.19")
+	// source = source.WithDirectory(
+	// 	"/app",
+	// 	client.Host().Directory("."),
+	// 	dagger.ContainerWithDirectoryOpts{
+	// 		Exclude: []string{"ci/"},
+	// 	},
+	// )
 
-	build := test.WithWorkdir("/app")
-	out, err = build.WithExec([]string{"go", "build", "-v", "-o", "shopa"}).Stderr(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(out)
+	// runner := source.WithWorkdir("/app")
+	// out, err := runner.WithExec([]string{"go", "mod", "download"}).Stderr(ctx)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(out)
 
-	dir := build.Directory("/app")
-	e, err := dir.Entries(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("build dir contents:\n %s\n", e)
+	// test := runner.WithWorkdir("/app")
+	// out, err = test.WithExec([]string{"go", "test", "./..."}).Stderr(ctx)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(out)
+
+	// build := test.WithWorkdir("/app")
+	// out, err = build.WithExec([]string{"go", "build", "-v", "-o", "shopa"}).Stderr(ctx)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(out)
+
+	// dir := build.Directory("/app")
+	// e, err := dir.Entries(ctx)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("build dir contents:\n %s\n", e)
 
 }
