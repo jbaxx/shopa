8b4cf95fcb9dd0fcf6f9ff6bb0b5476090b94c6d
jbaxx <jbaxx@users.noreply.github.com>
2023-09-12 22:56:17 -0500

    Multi goos and goarch pipeline

--- ci/main.go
+++ ci/main.go
@@ -23,6 +23,11 @@ func main() {
 func build(ctx context.Context) error {
 	fmt.Println("Building with Dagger")
 
+	// define build matrix
+	oses := []string{"linux", "darwin"}
+	arches := []string{"amd64", "arm64"}
+
+	// initialize Dagger client
 	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
 	if err != nil {
 		return err
@@ -32,20 +37,38 @@ func build(ctx context.Context) error {
 	// get reference to the local project
 	src := client.Host().Directory(".")
 
+	// create empty directory to put the build outputs
+	outputs := client.Directory()
+
 	golang := client.Container().From("golang:latest")
 
 	// mount cloned repository into `golang` image
 	golang = golang.WithDirectory("/app", src).WithWorkdir("/app")
 
-	// define the application build command
-	path := "build/"
-	golang = golang.WithExec([]string{"go", "build", "-v", "-o", path})
+	for _, goos := range oses {
+		for _, goarch := range arches {
+			// create a directory for each os and arch
+			path := fmt.Sprintf("build/%s/%s/", goos, goarch)
 
-	// get a reference to build output directory in container
-	output := golang.Directory(path)
+			// set GOARCH and GOOS in the build environment
+			build := golang.WithEnvVariable("GOOS", goos)
+			build = build.WithEnvVariable("GOARCH", goarch)
 
-	// write contents of container build/ directory to the host
-	_, err = output.Export(ctx, path)
+			// build application
+			build = build.WithExec([]string{"go", "build", "-v", "-o", path})
+
+			// get reference to build output directory in container
+			// WithDirectory write a directory at the given path,
+			// with the contents of the passed dir.
+			// Directory retrieves a directory.
+			// This is creating new paths (to build the tree),
+			// with the contents of the current build path.
+			outputs = outputs.WithDirectory(path, build.Directory(path))
+		}
+	}
+
+	// write build artifacts to host
+	_, err = outputs.Export(ctx, ".")
 	if err != nil {
 		return err
 	}
