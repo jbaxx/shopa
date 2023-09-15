3243dc29009e5160385a92cfb8458217a97b80bc
jbaxx <jbaxx@users.noreply.github.com>
2023-09-14 21:22:52 -0500

    Add descriptive error messages

--- ci/main.go
+++ ci/main.go
@@ -10,17 +10,21 @@ import (
 
 func main() {
 	ctx := context.Background()
-	test(ctx)
+	if err := test(ctx); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
 
 func test(ctx context.Context) error {
 	fmt.Println("Testing with Dagger")
 
-	goVersions := []string{"1.20", "1.21"}
+	goVersions := []string{"1.20"}
+	const workDir = "/app"
 
 	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
 	if err != nil {
-		return err
+		return fmt.Errorf("dagger connect: %w", err)
 	}
 	defer client.Close()
 
@@ -32,39 +36,46 @@ func test(ctx context.Context) error {
 		golang := client.Container().From(imageTag)
 
 		// mount local project into the golang image
-		golang = golang.WithDirectory("/app", src)
+		golang = golang.WithDirectory(workDir, src)
+
+		dir := golang.Directory(workDir)
+		e, err := dir.Entries(ctx)
+		if err != nil {
+			return fmt.Errorf("dagger entries: %w", err)
+		}
+		fmt.Printf("Contents of work dir %s:\n%s\n", workDir, e)
 
 		// install dependencies
-		runner := golang.WithWorkdir("/app")
+		runner := golang.WithWorkdir(workDir)
 		out, err := runner.WithExec([]string{"go", "mod", "download"}).Stderr(ctx)
 		if err != nil {
-			return err
+			return fmt.Errorf("dagger dependencies install: %w", err)
 		}
 		fmt.Println(out)
 
 		// run tests
-		test := runner.WithWorkdir("/app")
+		test := runner.WithWorkdir(workDir)
 		out, err = test.WithExec([]string{"go", "test", "./..."}).Stderr(ctx)
 		if err != nil {
-			return err
+			return fmt.Errorf("dagger tests: %w", err)
 		}
 		fmt.Println(out)
 
 		// run vulnerability checks
-		vuln := test.WithWorkdir("/app")
+		vuln := test.WithWorkdir(workDir)
 		out, err = vuln.WithExec([]string{
 			"go",
 			"install",
 			"golang.org/x/vuln/cmd/govulncheck@latest",
 		}).Stderr(ctx)
 		if err != nil {
-			return err
+			return fmt.Errorf("dagger govulncheck install: %w", err)
 		}
 		fmt.Println(out)
 
 		out, err = test.WithExec([]string{"govulncheck", "./..."}).Stderr(ctx)
 		if err != nil {
-			return err
+			return fmt.Errorf("dagger govulncheck: %w", err)
 		}
 		fmt.Println(out)
 
