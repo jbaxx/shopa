5ba70cfc338cb113f86f0aa8a58b01f5aafcbecf
jbaxx <jbaxx@users.noreply.github.com>
2023-09-14 22:07:10 -0500

    Set path

--- ci/main.go
+++ ci/main.go
@@ -41,9 +41,11 @@ func test(ctx context.Context) error {
 		golang := client.Container().From(imageTag).WithMountedCache("/app/node", depCache)
 
 		// mount local project into the golang image
-		golang = golang.WithDirectory(workDir, src)
+		golang = golang.WithDirectory(workDir, src).
+			WithEnvVariable("GOPATH", "/go").
+			WithEnvVariable("PATH", "$PATH:$GOPATH/bin")
 
-		dir := golang.Directory("/")
+		dir := golang.Directory("/go")
 		e, err := dir.Entries(ctx)
 		if err != nil {
 			return fmt.Errorf("dagger entries: %w", err)
